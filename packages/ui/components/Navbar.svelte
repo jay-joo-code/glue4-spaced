331ca9ed68cc886a29f31d7421c8e9e355b84485
Jay Joo <jj534@cornell.edu>
2024-02-26 12:13:40 -0500

    dynamically generate nav elements

--- packages/ui/components/Navbar.svelte
+++ packages/ui/components/Navbar.svelte
@@ -1,61 +1,76 @@
 <script lang="ts">
-	export let appName: string;
-	export let sitemap = [];
-	export let actionButtons = [];
+  export let appName: string;
+  export let sitemap = [];
+  export let actionButtons = [];
 </script>
 
-<div class="navbar bg-base-100">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<div tabindex="0" role="button" class="btn btn-ghost md:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</div>
-			<ul
-				tabindex="0"
-				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 w-52 rounded-box"
-			>
-				<li><a>Item 1</a></li>
-				<li>
-					<a>Parent</a>
-					<ul class="p-2">
-						<li><a>Submenu 1</a></li>
-						<li><a>Submenu 2</a></li>
-					</ul>
-				</li>
-				<li><a>Item 3</a></li>
-			</ul>
-		</div>
+<div class="navbar bg-base-100 z-20 relative">
+  <div class="navbar-start">
+    <!-- mobile burger nav -->
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost md:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          /></svg
+        >
+      </div>
 
-		<a class="btn btn-ghost text-xl">{appName}</a>
-	</div>
-	<div class="navbar-center hidden md:flex">
-		<ul class="menu menu-horizontal px-1">
-			<li><a>Item 1</a></li>
-			<li>
-				<details>
-					<summary>Parent</summary>
-					<ul class="p-2">
-						<li><a>Submenu 1</a></li>
-						<li><a>Submenu 2</a></li>
-					</ul>
-				</details>
-			</li>
-			<li><a>Item 3</a></li>
-		</ul>
-	</div>
-	<div class="navbar-end">
-		<a class="btn">Button</a>
-	</div>
+      <ul
+        tabindex="0"
+        class="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 w-52 rounded-box"
+      >
+        {#each sitemap as navElt}
+          {#if navElt?.isParent}
+            <li>
+              <a>{navElt?.label}</a>
+              <ul class="p-2">
+                {#each navElt?.children as childNavElt}
+                  <li><a href={childNavElt?.href}>{childNavElt?.label}</a></li>
+                {/each}
+              </ul>
+            </li>
+          {:else}
+            <li><a href={navElt?.href}>{navElt?.label}</a></li>
+          {/if}
+        {/each}
+      </ul>
+    </div>
+
+    <a class="btn btn-ghost px-1 text-xl" href="/">{appName}</a>
+  </div>
+
+  <!-- desktop -->
+  <div class="navbar-center hidden md:flex">
+    <ul class="menu menu-horizontal px-1">
+      {#each sitemap as navElt}
+        {#if navElt?.isParent}
+          <li>
+            <details>
+              <summary>{navElt?.label}</summary>
+              <ul class="menu p-2">
+                {#each navElt?.children as childNavElt}
+                  <li><a href={childNavElt?.href}>{childNavElt?.label}</a></li>
+                {/each}
+              </ul>
+            </details>
+          </li>
+        {:else}
+          <li><a href={navElt?.href}>{navElt?.label}</a></li>
+        {/if}
+      {/each}
+    </ul>
+  </div>
+
+  <div class="navbar-end">
+    <a class="btn">Button</a>
+  </div>
 </div>
