d090e8ba2dfd08e42c6ac104abaec010dc33ab62
Jay Joo <jj534@cornell.edu>
2024-02-26 11:20:49 -0500

    remove unused web app

--- apps/web/.eslintrc.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	extends: ['@glue/config-eslint/index.js']
-};
--- apps/web/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { MyCounterButton } from '@glue/ui';
-</script>
-
-<h1>Web</h1>
-<MyCounterButton />
-<p class="">new text on web page</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
