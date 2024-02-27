484d61dc64d927505fbe71557689b2109019ab3e
Jay Joo <jj534@cornell.edu>
2024-02-26 23:02:40 -0500

    add navigation panel

--- apps/onesentence-editor/src/routes/sitemap/[projectId]/+page.svelte
+++ apps/onesentence-editor/src/routes/sitemap/[projectId]/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import PageItem from '$root/src/lib/components/PageItem.svelte';
-	import PageContainer from '$root/src/lib/components/glue/PageContainer.svelte';
+  import { page } from '$app/stores';
+  import PageItem from '$root/src/lib/components/PageItem.svelte';
+  import PageContainer from '$root/src/lib/components/glue/PageContainer.svelte';
+  import { IconMap, IconPalette, IconRocket, IconCube } from '@glue/ui';
 
-	$: ({ project } = $page?.data);
-	$: console.log('project', project);
+  $: ({ project } = $page?.data);
 </script>
 
 <PageContainer title="Edit project" limitWidth={false}>
-	<div class="flex min-h-[80vh] w-full items-center justify-center">
-		{#each project?.expand['pages(project)'] as page}
-			<PageItem {page} />
-		{/each}
-	</div>
+  <div class="flex min-h-[80vh] w-full items-center justify-center relative">
+    <div class="absolute top-10 left-0 right-0 flex justify-center">
+      <ul class="menu menu-horizontal bg-base-200 rounded-box">
+        <li>
+          <a href="/sitemap/${project?.id}"><span class="text-lg"><IconMap /></span>Sitemap</a>
+        </li>
+        <li>
+          <a href="/design/${project?.id}"><span class="text-lg"><IconCube /></span>Design</a>
+        </li>
+        <li>
+          <a href="/theme/${project?.id}"><span class="text-lg"><IconPalette /></span>Theme</a>
+        </li>
+        <li>
+          <a href="/publish/${project?.id}"><span class="text-lg"><IconRocket /></span>Publish</a>
+        </li>
+      </ul>
+    </div>
+
+    {#each project?.expand['pages(project)'] as page}
+      <PageItem {page} />
+    {/each}
+  </div>
 </PageContainer>
