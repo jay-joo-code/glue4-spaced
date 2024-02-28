db79ef47a0b13d16ed71171808ae15c6521e8d9a
Jay Joo <jj534@cornell.edu>
2024-02-27 20:34:01 -0500

    improve hero section styles

--- packages/ui/components/SectionHero.svelte
+++ packages/ui/components/SectionHero.svelte
@@ -5,19 +5,27 @@
 </script>
 
 <div class="w-screen flex flex-col justify-center items-center max-w-full aspect-video">
-  {#if cpOverline}
-    <p class="text-sm font-bold mb-4">
-      {cpOverline?.label}
-    </p>
-  {/if}
+  <div class="text-center w-2/3 flex flex-col items-center">
+    {#if cpOverline}
+      <div class=" flex items-center justify-center mb-4">
+        <div class="py-2 px-6 rounded-full border border-base-content/10">
+          <p class="text-xs text-base-content/70 font-semibold">
+            {cpOverline?.label}
+          </p>
+        </div>
+      </div>
+    {/if}
 
-  {#if cpTitle}
-    <p class="text-2xl font-extrabold tracking-wide">
-      {cpTitle?.label}
-    </p>
-  {/if}
+    {#if cpTitle}
+      <p class="text-4xl font-extrabold tracking-wide leading-normal">
+        {cpTitle?.label}
+      </p>
+    {/if}
 
-  {#if cpDesc}
-    <p class="mt-4 font-bold">{cpDesc?.label}</p>
-  {/if}
+    {#if cpDesc}
+      <div class="mt-4 w-[80%]">
+        <p class="font-medium text-base-content/70 text-sm leading-normal">{cpDesc?.label}</p>
+      </div>
+    {/if}
+  </div>
 </div>
