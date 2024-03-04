bb764c57b22c40095c8ce0fdbcd1972f2f461382
Jay Joo <jj534@cornell.edu>
2024-03-04 02:23:54 -0500

    implement ui item dnd mvp

--- packages/ui/components/DynamicSection.svelte
+++ packages/ui/components/DynamicSection.svelte
@@ -1,42 +1,64 @@
 <script lang="ts">
+  import Grid from 'svelte-grid';
+  import gridHelp from 'svelte-grid/build/helper/index.mjs';
+
   export let section;
   $: console.log('section', section);
+
+  const id = () => '_' + Math.random().toString(36).substr(2, 9);
+
+  let items = [
+    {
+      24: gridHelp.item({
+        x: 2,
+        y: 8,
+        w: 10
+      }),
+      id: id(),
+      variant: 'TEXT',
+      value: 'The modern way of carpooling',
+      styles: {
+        fontSize: 'text-7xl',
+        fontWeight: 'font-extrabold',
+        lineHeight: 'leading-tight'
+      }
+    },
+
+    {
+      24: gridHelp.item({
+        x: 2,
+        y: 14,
+        w: 10
+      }),
+      id: id(),
+      variant: 'TEXT',
+      value:
+        'A carpool app facilitates ride-sharing by connecting drivers with passengers heading in the same direction, promoting cost-effective and eco-friendly transportation solutions. Users can schedule rides, split costs, and reduce traffic congestion through this convenient platform.',
+      styles: {
+        fontSize: 'text-md',
+        fontWeight: 'font-normal',
+        textColor: 'text-base-content/70',
+        lineHeight: 'leading-loose'
+      }
+    }
+  ];
+
+  $: console.log('items', items);
 </script>
 
 {#if section}
-  <div
-    class="border border-blue-500 w-full grid-container"
-    style="--desktop-row-count:{section?.desktopRowCount};"
-  >
-    <!-- TODO: responsive row count -->
-    {#each [...Array(section?.desktopRowCount * 26).keys()] as _}
-      <div class="border border-base-content/10 rounded"></div>
-    {/each}
+  <div class="border border-transparent hover:border-blue-600 w-full min-h-[80vh]">
+    <Grid bind:items gap={[11, 11]} rowHeight={40} let:item let:dataItem cols={[[1100, 24]]}>
+      <div class="hover:border-blue-400 border border-transparent w-full h-full">
+        <div
+          class="border-2 border-transparent hover:border-blue-400 w-full"
+          contenteditable={true}
+        >
+          <h2 class={Object.values(dataItem?.styles)?.join(' ')}>
+            {dataItem.value}
+          </h2>
+        </div>
+      </div>
+    </Grid>
   </div>
 {/if}
-
-<style scoped>
-  .grid-container {
-    display: grid;
-    row-gap: 11px;
-    column-gap: 11px;
-
-    /* desktop */
-    --grid-gutter: calc(var(--sqs-site-gutter, 4vw) - 11px);
-    --cell-max-width: calc((var(--sqs-site-max-width, 1500px) - (11px * 23)) / 24);
-    --inset-padding: 0vw;
-    --container-width: min(
-      var(--sqs-site-max-width, 1500px),
-      calc(100vw - var(--sqs-site-gutter, 4vw) * 2 - var(--inset-padding))
-    );
-
-    /* TODO: --mobile-row-count */
-    grid-template-rows: repeat(
-      var(--desktop-row-count),
-      minmax(calc(var(--container-width) * 0.0215), auto)
-    );
-    grid-template-columns:
-      minmax(var(--grid-gutter), 1fr) repeat(24, minmax(0, var(--cell-max-width)))
-      minmax(var(--grid-gutter), 1fr);
-  }
-</style>
