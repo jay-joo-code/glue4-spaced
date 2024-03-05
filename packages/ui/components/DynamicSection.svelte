5ce08597d7b64596de6459577bae6dc5c5f3dfb7
Jay Joo <jj534@cornell.edu>
2024-03-04 21:20:56 -0500

    implement text styles edit panel

--- packages/ui/components/DynamicSection.svelte
+++ packages/ui/components/DynamicSection.svelte
@@ -51,14 +51,50 @@
 {#if section}
   <div class="border border-transparent hover:border-blue-600 w-full min-h-[80vh]">
     <Grid bind:items gap={[11, 11]} rowHeight={30} let:item let:dataItem cols={[[1100, 24]]}>
-      <div class="hover:border-blue-400 border border-transparent w-full h-full">
-        <div
-          class="border-2 border-transparent hover:border-blue-400 w-full"
-          contenteditable={true}
-        >
-          <h2 class={Object.values(dataItem?.styles)?.join(' ')}>
-            {dataItem.value}
-          </h2>
+      <div class="w-full h-full">
+        <div class="dropdown dropdown-bottom dropdown-end">
+          <div
+            class="focus:border-blue-400 border border-transparent w-full mb-2 rounded p-1"
+            contenteditable={true}
+            tabindex="0"
+            role="button"
+          >
+            <h2 class={Object.values(dataItem?.styles)?.join(' ')}>
+              {dataItem.value}
+            </h2>
+          </div>
+
+          <!-- edit styles panel -->
+          <ul
+            tabindex="0"
+            class="dropdown-content z-20 px-4 py-6 shadow bg-base-300 rounded-box w-52 border border-base-content/20"
+          >
+            <h3 class="uppercase text-sm font-extrabold mb-3 ml-1 text-base-content/80">Text</h3>
+            <button
+              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+              on:click={() => {
+                dataItem.styles.fontSize = 'text-7xl';
+              }}>Heading 1</button
+            >
+            <button
+              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+              on:click={() => {
+                dataItem.styles.fontSize = 'text-3xl';
+              }}>Heading 2</button
+            >
+            <button
+              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+              on:click={() => {
+                dataItem.styles.fontSize = 'text-md';
+              }}>Body</button
+            >
+            <button
+              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+              on:click={() => {
+                dataItem.styles.fontSize = 'text-sm';
+              }}>Meta</button
+            >
+          </ul>
         </div>
       </div>
     </Grid>
