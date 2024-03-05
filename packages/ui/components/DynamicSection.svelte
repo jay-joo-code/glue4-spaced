e168ed3182e5306dce97e61d0e846b3a0d2dac2f
Jay Joo <jj534@cornell.edu>
2024-03-04 22:45:21 -0500

    implement improved style edit panel

--- packages/ui/components/DynamicSection.svelte
+++ packages/ui/components/DynamicSection.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Grid from 'svelte-grid';
   import gridHelp from 'svelte-grid/build/helper/index.mjs';
+  import { clickOutsideAction } from '@glue/utils';
 
   export let section;
-  $: console.log('section', section);
 
+  let selectedItemId = null;
   const id = () => '_' + Math.random().toString(36).substr(2, 9);
 
   let items = [
@@ -45,60 +46,76 @@
     }
   ];
 
-  $: console.log('items', items);
+  const updateSelectedItemStyles = ({ styleKey, styleValue }) => {
+    items = items.map((item) => {
+      if (item.id !== selectedItemId) return item;
+      item.styles[styleKey] = styleValue;
+      return item;
+    });
+  };
 </script>
 
 {#if section}
-  <div class="border border-transparent hover:border-blue-600 w-full min-h-[80vh]">
+  <div class="border border-transparent hover:border-blue-400 w-full min-h-[80vh]">
     <Grid bind:items gap={[11, 11]} rowHeight={30} let:item let:dataItem cols={[[1100, 24]]}>
       <div class="w-full h-full">
-        <div class="dropdown dropdown-bottom dropdown-end">
-          <div
-            class="focus:border-blue-400 border border-transparent w-full mb-2 rounded p-1"
-            contenteditable={true}
-            tabindex="0"
-            role="button"
-          >
-            <h2 class={Object.values(dataItem?.styles)?.join(' ')}>
-              {dataItem.value}
-            </h2>
-          </div>
-
-          <!-- edit styles panel -->
-          <ul
-            tabindex="0"
-            class="dropdown-content z-20 px-4 py-6 shadow bg-base-300 rounded-box w-52 border border-base-content/20"
-          >
-            <h3 class="uppercase text-sm font-extrabold mb-3 ml-1 text-base-content/80">Text</h3>
-            <button
-              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
-              on:click={() => {
-                dataItem.styles.fontSize = 'text-7xl';
-              }}>Heading 1</button
-            >
-            <button
-              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
-              on:click={() => {
-                dataItem.styles.fontSize = 'text-3xl';
-              }}>Heading 2</button
-            >
-            <button
-              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
-              on:click={() => {
-                dataItem.styles.fontSize = 'text-md';
-              }}>Body</button
-            >
-            <button
-              class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
-              on:click={() => {
-                dataItem.styles.fontSize = 'text-sm';
-              }}>Meta</button
-            >
-          </ul>
+        <div
+          class="focus:border-blue-400 border border-transparent w-full rounded p-1"
+          contenteditable={true}
+          on:focus={() => {
+            // HACK: setTimeout used to run after outside click handler
+            setTimeout(() => {
+              selectedItemId = dataItem.id;
+            });
+          }}
+        >
+          <h2 class={Object.values(dataItem?.styles)?.join(' ')}>
+            {dataItem.value}
+          </h2>
         </div>
       </div>
     </Grid>
   </div>
+
+  <!-- edit styles panel -->
+  {#if selectedItemId}
+    <div
+      class="fixed right-4 top-[50%] -translate-y-1/2 z-20"
+      use:clickOutsideAction
+      on:click_outside={() => {
+        selectedItemId = null;
+      }}
+    >
+      <div class="px-4 py-6 shadow bg-base-300 rounded-box w-52 border border-base-content/20">
+        <h3 class="uppercase text-sm font-extrabold mb-3 ml-1 text-base-content/80">Text</h3>
+        <button
+          class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+          on:click={(event) => {
+            updateSelectedItemStyles({ styleKey: 'fontSize', styleValue: 'text-7xl' });
+          }}
+          >Heading 1
+        </button>
+        <button
+          class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+          on:click={() => {
+            updateSelectedItemStyles({ styleKey: 'fontSize', styleValue: 'text-3xl' });
+          }}>Heading 2</button
+        >
+        <button
+          class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+          on:click={() => {
+            updateSelectedItemStyles({ styleKey: 'fontSize', styleValue: 'text-md' });
+          }}>Body</button
+        >
+        <button
+          class="btn btn-ghost btn-block text-left justify-start btn-sm pl-1"
+          on:click={() => {
+            updateSelectedItemStyles({ styleKey: 'fontSize', styleValue: 'text-sm' });
+          }}>Meta</button
+        >
+      </div>
+    </div>
+  {/if}
 {/if}
 
 <style>
