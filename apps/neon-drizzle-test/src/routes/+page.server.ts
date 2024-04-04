585a7f416b98ebe9d96f8129f4fa7e9f1f8f63cd
Jay Joo <jj534@cornell.edu>
2024-04-03 21:57:41 -0400

    upgrade to sveltekit v2

--- apps/neon-drizzle-test/src/routes/+page.server.ts
+++ apps/neon-drizzle-test/src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import type { ServerLoad } from '@sveltejs/kit';
-import db from '$lib/db/drizzle.server';
-import { todo } from '$lib/db/schema.server';
+import db from '$src/db/drizzle.server';
+import { todo } from '$src/db/schema.server';
 
 export const load: ServerLoad = async ({}) => {
   const fetchTodos = async () => {
--- apps/neon-drizzle-test/src/routes/+page.svelte
+++ apps/neon-drizzle-test/src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+  import type { PageData } from './$types';
 
-  $: ({ todos } = $page.data);
-  $: console.log('todos', todos);
+  export let data: PageData;
 </script>
+
+{#await data.todos}
+  <div class="skeleton w-32 h-32" />
+{:then todos}
+  {#each todos as todo}
+    <div class="p-4 rounded border border-base-content/10">
+      <p class="">{todo.text}</p>
+    </div>
+  {/each}
+{/await}
