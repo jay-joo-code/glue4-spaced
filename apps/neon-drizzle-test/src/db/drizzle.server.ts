e3565c1708eac91a3c36c390526764289930714c
Jay Joo <jj534@cornell.edu>
2024-04-05 14:10:27 -0400

    connect to pooled DB in dev env

--- apps/neon-drizzle-test/src/db/drizzle.server.ts
+++ apps/neon-drizzle-test/src/db/drizzle.server.ts
@@ -1,8 +1,9 @@
-import { NEON_DATABASE_URL_CACHED } from '$env/static/private';
+import { dev } from '$app/environment';
+import { NEON_DATABASE_URL_CACHED, NEON_DATABASE_URL_POOLED } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const queryClient = postgres(NEON_DATABASE_URL_CACHED);
+const queryClient = postgres(dev ? NEON_DATABASE_URL_POOLED : NEON_DATABASE_URL_CACHED);
 const db = drizzle(queryClient);
 
 export default db;
