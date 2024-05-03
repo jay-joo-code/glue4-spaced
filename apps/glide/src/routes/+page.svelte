e54c611a612d02a19db50f708add9e5a993b28f9
Jay Joo <jj534@cornell.edu>
2024-05-03 01:11:43 -0400

    fetch user cookie data in layout load

--- apps/glide/src/routes/+page.svelte
+++ apps/glide/src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { APP_NAME } from '$lib/config';
   import { PageContainer } from '@glue/ui';
+
+  export let data;
+
+  $: console.log('data.user', data.user);
 </script>
 
 <PageContainer {APP_NAME} title="Home">
