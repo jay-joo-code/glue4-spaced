3f0e8f805e65465aa48e4662cb4255ac0a39a312
Jay Joo <jj534@cornell.edu>
2024-05-05 16:26:36 -0400

    wip - plaid integration

--- apps/glide/src/routes/+page.server.ts
+++ apps/glide/src/routes/+page.server.ts
@@ -1,8 +1,10 @@
 import { protectedRouteRedirectUrl } from '$root/src/lib/util/auth';
-import { redirect, type ServerLoad } from '@sveltejs/kit';
+import { redirect, type Actions, type ServerLoad } from '@sveltejs/kit';
 import db from '../db/drizzle.server';
 import { itemTable } from '../db/schema.server';
 import { eq } from 'drizzle-orm';
+import { APP_NAME } from '../lib/config';
+import { PLAID_CLIENT_ID, PLAID_SECRET_SANDBOX } from '$env/static/private';
 
 export const load: ServerLoad = async ({ url, locals }) => {
   if (!locals.user) {
@@ -25,3 +27,40 @@ export const load: ServerLoad = async ({ url, locals }) => {
     items: fetchItems()
   };
 };
+
+export const actions = {
+  createPlaidToken: async ({ fetch, locals, url }) => {
+    if (!locals.user) return;
+
+    const TOKEN_CREATE_URL = 'https://sandbox.plaid.com/link/token/create';
+    const response = await fetch(TOKEN_CREATE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: PLAID_CLIENT_ID,
+        secret: PLAID_SECRET_SANDBOX,
+        user: {
+          client_user_id: locals.user.id
+        },
+        client_name: 'Personal Finance App',
+        products: ['transactions'],
+        transactions: {
+          days_requested: 730
+        },
+        country_codes: ['US'],
+        language: 'en',
+        account_filters: {
+          depository: {
+            account_subtypes: ['checking', 'savings']
+          },
+          credit: {
+            account_subtypes: ['credit card']
+          }
+        }
+      })
+    });
+    return await response.json();
+  }
+} satisfies Actions;
--- apps/glide/src/routes/+page.svelte
+++ apps/glide/src/routes/+page.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { enhance } from '$app/forms';
   import { APP_NAME } from '$lib/config';
   import { IconAdd, PageContainer } from '@glue/ui';
 
   export let data;
+  export let form;
+
+  let isLoadingCreateIntegration = false;
+
+  $: console.log('form', form);
+  $: if (browser && form?.link_token) {
+    const handler = window.Plaid?.create({
+      token: form?.link_token,
+      onSuccess: (public_token, metadata) => {
+        console.log('public_token, metadata', public_token, metadata);
+        fetch('/api/plaid/create-item', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            public_token,
+            account_id: metadata.account_id,
+            institution: metadata.institution.name
+          })
+        });
+      },
+      onLoad: () => {
+        console.log('onLoad');
+      },
+      onExit: (err, metadata) => {
+        console.log('err, metadata', err, metadata);
+      },
+      onEvent: (eventName, metadata) => {
+        console.log('eventName, metadata', eventName, metadata);
+      }
+    });
+    handler.open();
+  }
 </script>
 
 <PageContainer {APP_NAME} title="Home">
   <section class="py-12">
     <div class="flex justify-between items-center">
       <p class="text-4xl font-extrabold">Integrations</p>
-      <button class="btn btn-primary btn-sm">
-        <IconAdd />
-        Add integration</button
-      >
+      <form method="POST" action="?/createPlaidToken" use:enhance>
+        <button class="btn btn-primary btn-sm">
+          {#if isLoadingCreateIntegration}
+            <span class="loading loading-spinner loading-sm" />
+          {:else}
+            <IconAdd />
+            Add integration
+          {/if}
+        </button>
+      </form>
     </div>
     <div class="mt-6">
       {#await data.items}
