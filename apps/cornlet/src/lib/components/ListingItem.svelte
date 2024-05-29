643ff9c7360612e99c648b0dc65473506f8a5d51
Jay Joo <jj534@cornell.edu>
2024-05-29 16:41:12 -0400

    init listing item component

---
+++ apps/cornlet/src/lib/components/ListingItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { SelectListing } from '$root/src/db/schema';
+
+  export let listing: SelectListing;
+</script>
+
+<div class="grid grid-cols-6">
+  <div class="col-span-2">
+    <img class="w-full object-cover rounded-lg" src={listing.photoUrls[0]} />
+  </div>
+
+  <div class="col-span-6">
+    <h2 class="font-extrabold text-xl">
+      {listing.availableRooms} in a {listing.totalRooms}-BR {listing.propertyType}
+    </h2>
+  </div>
+</div>
