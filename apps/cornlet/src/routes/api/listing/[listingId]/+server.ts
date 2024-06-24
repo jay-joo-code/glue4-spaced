2a2ae77ff331debb736e6f7198f0e136ce88e0cb
Jay Joo <jj534@cornell.edu>
2024-06-24 09:08:01 -0400

    improve code style

--- apps/cornlet/src/routes/api/listing/[listingId]/+server.ts
+++ apps/cornlet/src/routes/api/listing/[listingId]/+server.ts
@@ -1,26 +1,32 @@
-import { json, redirect } from '@sveltejs/kit';
+import { json, redirect, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { protectedRouteRedirectUrl } from '$root/src/lib/util/auth';
 import db from '$root/src/db/drizzle.server';
 import { listingTable } from '$root/src/db/schema';
 import { and, eq } from 'drizzle-orm';
-import { error } from '@sveltejs/kit';
 
 export const PUT: RequestHandler = async ({ request, url, params, locals }) => {
-  if (!locals.user) return redirect(302, protectedRouteRedirectUrl(url));
+  if (!locals.user) {
+    return redirect(302, protectedRouteRedirectUrl(url));
+  }
 
   try {
     const requestBody = await request.json();
     const { listingId } = params;
 
-    const listing = await db
+    const [listing] = await db
       .update(listingTable)
       .set(requestBody)
       .where(and(eq(listingTable.id, listingId), eq(listingTable.userId, locals.user.id)))
       .returning();
 
+    if (!listing) {
+      throw error(404, 'Listing not found or you do not have permission to update it');
+    }
+
     return json(listing);
   } catch (e) {
-    error(500);
+    console.error('Error updating listing:', e);
+    throw error(500, 'Internal Server Error');
   }
 };
