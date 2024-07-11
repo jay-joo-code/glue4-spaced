7592793ea35fbfcb95a8d1ec08bf786d432fae7c
Jay Joo <jj534@cornell.edu>
2024-07-11 14:59:04 -0400

    fix dnd bug

--- packages/ui/components/FileUploadInput.svelte
+++ packages/ui/components/FileUploadInput.svelte
@@ -1,4 +1,5 @@
 <script lang="ts" generics="T extends Record<string, unknown>">
+  import { onMount } from 'svelte';
   import FormHelperText from './FormHelperText.svelte';
   import type { HandleFileUpload, HelperText, HelperTextStatus } from '@glue/types';
   import type { DndEvent, Item } from 'svelte-dnd-action';
@@ -19,22 +20,29 @@
   const { values, errors } = arrayProxy(superform, field);
   let isLoading = false;
   const flipDurationMs = 300;
+  let items = [];
 
   const handleDndConsider = (event: CustomEvent<DndEvent<Item>>) => {
-    $values = event.detail.items.map((item) => item.id);
+    items = event.detail.items;
   };
   const handleDndFinalize = (event: CustomEvent<DndEvent<Item>>) => {
+    items = event.detail.items;
     $values = event.detail.items.map((item) => item.id);
   };
   const onFileUpload = async (event: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
     if (event.currentTarget.files) {
       isLoading = true;
       const newUrls = await handleFileUpload({ files: event.currentTarget.files, superform });
+      items = [...items, ...newUrls.map((url) => ({ id: url }))];
       // @ts-expect-error: type gymnastics
       $values = [...$values, ...newUrls];
       isLoading = false;
     }
   };
+
+  onMount(() => {
+    items = $values.map((url) => ({ id: url }));
+  });
 </script>
 
 <label class="form-control w-full">
@@ -69,11 +77,11 @@
 
 <div
   class="grid grid-cols-2 gap-4 mt-8 !outline-none lg:grid-cols-3 xl:grid-cols-4"
-  use:dndzone={{ items: $values.map((url) => ({ id: url })), flipDurationMs }}
+  use:dndzone={{ items, flipDurationMs }}
   on:consider={handleDndConsider}
   on:finalize={handleDndFinalize}
 >
-  {#each $values.map((url) => ({ id: url })) as item (item.id)}
+  {#each items as item (item.id)}
     <div class="relative" animate:flip={{ duration: flipDurationMs }}>
       <img
         class="object-cover w-full rounded-lg border border-base-content/10 hover:shadow-xl hover:opacity-70 aspect-[4/3]"
@@ -82,7 +90,8 @@
       <button
         class="absolute -right-2 -top-2 btn-xs text-[0.9rem] btn btn-circle pb-1 border border-base-content/10"
         on:click={(event) => {
-          $values = $values.filter((existingUrl) => existingUrl !== item.id);
+          items = items.filter((existingItem) => existingItem.id !== item.id);
+          $values = items.map((item) => item.id);
         }}
       >
         <!-- NOTE: Using svg in children of dnd action throws errors -->
