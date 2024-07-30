fe263e599a20c7fd57217897ae14114bf25b0189
Jay Joo <jj534@cornell.edu>
2024-07-30 18:08:11 -0400

    move pagecontainer to signinpage

--- apps/glide/src/routes/login/+page.svelte
+++ apps/glide/src/routes/login/+page.svelte
@@ -1,13 +1,11 @@
 <script>
-  import { APP_NAME } from '$root/src/lib/config';
-  import { PageContainer, SignInPage } from '@glue/ui';
   import { page } from '$app/stores';
+  import { APP_NAME } from '$root/src/lib/config';
+  import { SignInPage } from '@glue/ui';
 </script>
 
-<PageContainer {APP_NAME} title="Login" layout="fullscreen">
-  <SignInPage
-    {APP_NAME}
-    bgPattern="autumn"
-    redirectTo={$page.url.searchParams.get('redirectTo') ?? '/'}
-  />
-</PageContainer>
+<SignInPage
+  {APP_NAME}
+  bgPattern="autumn"
+  redirectTo={$page.url.searchParams.get('redirectTo') ?? '/'}
+/>
