c2c48f0d9f126c36183b588117c2e73ec9d3e9e2
Jay Joo <jj534@cornell.edu>
2024-07-31 08:36:02 -0400

    implement neon db migration

---
+++ apps/spaced/src/routes/+page.server.ts
@@ -0,0 +1,41 @@
+import db from '$lib/glue/db/drizzle.server';
+import { flashcardTable } from '$lib/glue/db/schema.server';
+import { protectedRouteRedirectUrl } from '@glue/utils';
+import { redirect, type ServerLoad } from '@sveltejs/kit';
+import { and, asc, desc, eq, gt, lte } from 'drizzle-orm';
+
+export const load: ServerLoad = async ({ url, locals }) => {
+  if (!locals.user) {
+    return redirect(302, protectedRouteRedirectUrl(url));
+  }
+
+  const fetchTodayFlashcards = async () => {
+    if (!locals.user) return [];
+
+    const flashcards = await db
+      .select()
+      .from(flashcardTable)
+      .where(and(eq(flashcardTable.userId, locals.user.id), lte(flashcardTable.due, new Date())))
+      .orderBy(desc(flashcardTable.due));
+
+    return flashcards;
+  };
+
+  const fetchUpcomingFlashcards = async () => {
+    if (!locals.user) return [];
+
+    const flashcards = await db
+      .select()
+      .from(flashcardTable)
+      .where(and(eq(flashcardTable.userId, locals.user.id), gt(flashcardTable.due, new Date())))
+      .orderBy(asc(flashcardTable.due))
+      .limit(4);
+
+    return flashcards;
+  };
+
+  return {
+    todayFlashcards: fetchTodayFlashcards(),
+    upcomingFlashcards: fetchUpcomingFlashcards()
+  };
+};
